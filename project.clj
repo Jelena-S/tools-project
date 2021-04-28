d0ae5fdcbb13c9c9564b9aadb416904df554812d
Jelena Sreckovic <jsreckovic1@gmail.com>
2021-04-28 16:42:41 +0200

    Added dependencies for db

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [reaver "0.1.3"]]
+                 [reaver "0.1.3"]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [org.xerial/sqlite-jdbc "3.23.1"]]
   :repl-options {:init-ns tools-project.core})
