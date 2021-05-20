27dcc8873df3bfdf0e5ad17ecced4a7a441d8d42
Jelena Sreckovic <jsreckovic1@gmail.com>
2021-05-20 19:11:22 +0200

    Added dependency for ring

--- project.clj
+++ project.clj
@@ -6,5 +6,12 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [reaver "0.1.3"]
                  [org.clojure/java.jdbc "0.7.11"]
-                 [org.xerial/sqlite-jdbc "3.23.1"]]
-  :repl-options {:init-ns tools-project.core})
+                 [org.xerial/sqlite-jdbc "3.23.1"]
+                 [hiccup "1.0.5"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler tools-project.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
